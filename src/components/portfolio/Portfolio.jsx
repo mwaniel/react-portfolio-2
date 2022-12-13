6fb99b4e66a8068b1f4ffcea174c47eb7a940df1
mwaniel <mwaniel03@gmail.com>
2022-12-13 10:52:18 +0300

    portfolio items in an array

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,94 +1,77 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/me.jpg'
+import IMG1 from '../../assets/me-about.jpg'
+
+const data =[
+  {
+    id: 1,
+    image: IMG1,
+    title: 'newakska d sdjn anjsnskd  najsnsd jkands',
+    github: 'https://github.com',
+    demo : 'https://drible.com'
+  },
+  {
+    id: 2,
+    image: IMG1,
+    title: 'newakska d sdjn anjsnskd  najsnsd jkands',
+    github: 'https://github.com',
+    demo : 'https://drible.com'
+  },
+  {
+    id: 3,
+    image: IMG1,
+    title: 'newakska d sdjn anjsnskd  najsnsd jkands',
+    github: 'https://github.com',
+    demo : 'https://drible.com'
+  },
+  {
+    id: 4,
+    image: IMG1,
+    title: 'newakska d sdjn anjsnskd  najsnsd jkands',
+    github: 'https://github.com',
+    demo : 'https://drible.com'
+  },
+  {
+    id: 5,
+    image: IMG1,
+    title: 'newakska d sdjn anjsnskd  najsnsd jkands',
+    github: 'https://github.com',
+    demo : 'https://drible.com'
+  },
+  {
+    id: 6,
+    image: IMG1,
+    title: 'newakska d sdjn anjsnskd  najsnsd jkands',
+    github: 'https://github.com',
+    demo : 'https://drible.com'
+  },
+  
+
+]
 
 const Portfolio = () => {
   return (
     <section id='portfolio'>
-      <h5>My Resent Work</h5>
+      <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
       <div className='container portfolio__container'>
-        <article className='portfolio__item'>
-            <div className='portfolio__item-image'>
-              <img src={IMG1} alt='' />
-            </div>
-            <h3>This is a portfolio item title</h3>
-            <div className='portfolio__item-cta'>
-              <a href='https://github.com' className='btn' target='_blank'>Github</a>
-              <a href='https://dribble.com' className='btn btn-primary' target ='_blank'>Live demo </a>
-            </div>
-          </article>
-          <article className='portfolio__item'>
-            <div className='portfolio__item-image'>
-              <img src={IMG1} alt='' />
-            </div>
-            <h3>This is a portfolio item title</h3>
-            <div className='portfolio__item-cta'>
-              <a href='https://github.com' className='btn' target='_blank'>Github</a>
-              <a href='https://dribble.com' className='btn btn-primary' target ='_blank'>Live demo </a>
-            </div>
-          </article>
-          <article className='portfolio__item'>
-            <div className='portfolio__item-image'>
-              <img src={IMG1} alt='' />
-            </div>
-            <h3>This is a portfolio item title</h3>
-            <div className='portfolio__item-cta'>
-              <a href='https://github.com' className='btn' target='_blank'>Github</a>
-              <a href='https://dribble.com' className='btn btn-primary' target ='_blank'>Live demo </a>
-            </div>
-          </article>
-          <article className='portfolio__item'>
-            <div className='portfolio__item-image'>
-              <img src={IMG1} alt='' />
-            </div>
-            <h3>This is a portfolio item title</h3>
-            <div className='portfolio__item-cta'>
-              <a href='https://github.com' className='btn' target='_blank'>Github</a>
-              <a href='https://dribble.com' className='btn btn-primary' target ='_blank'>Live demo </a>
-            </div>
-          </article>
-          <article className='portfolio__item'>
-            <div className='portfolio__item-image'>
-              <img src={IMG1} alt='' />
-            </div>
-            <h3>This is a portfolio item title</h3>
-            <div className='portfolio__item-cta'>
-              <a href='https://github.com' className='btn' target='_blank'>Github</a>
-              <a href='https://dribble.com' className='btn btn-primary' target ='_blank'>Live demo </a>
-            </div>
-          </article>
-          <article className='portfolio__item'>
-            <div className='portfolio__item-image'>
-              <img src={IMG1} alt='' />
-            </div>
-            <h3>This is a portfolio item title</h3>
-            <div className='portfolio__item-cta'>
-              <a href='https://github.com' className='btn' target='_blank'>Github</a>
-              <a href='https://dribble.com' className='btn btn-primary' target ='_blank'>Live demo </a>
-            </div>
-          </article>
-          <article className='portfolio__item'>
-            <div className='portfolio__item-image'>
-              <img src={IMG1} alt='' />
-            </div>
-            <h3>This is a portfolio item title</h3>
-            <div className='portfolio__item-cta'>
-              <a href='https://github.com' className='btn' target='_blank'>Github</a>
-              <a href='https://dribble.com' className='btn btn-primary' target ='_blank'>Live demo </a>
-            </div>
-          </article>
-          <article className='portfolio__item'>
-            <div className='portfolio__item-image'>
-              <img src={IMG1} alt='' />
-            </div>
-            <h3>This is a portfolio item title</h3>
-            <div className='portfolio__item-cta'>
-              <a href='https://github.com' className='btn' target='_blank'>Github</a>
-              <a href='https://dribble.com' className='btn btn-primary' target ='_blank'>Live demo </a>
-            </div>
-          </article>
+          {
+            data.map(({id, image, title, github, demo}) =>{
+              return(
+                  <article  key={id} className='portfolio__item'>
+                    <div className='portfolio__item-image'>
+                      <img src={image} alt={title} />
+                    </div>
+                    <h3>{title}</h3>
+                    <div className='portfolio__item-cta'>
+                      <a href={github} className='btn' target='_blank'>Github</a>
+                      <a href={demo} className='btn btn-primary' target ='_blank'>Live demo </a>
+                    </div>
+                  </article>
+              )
+            })
+          }
       </div>
     </section>
   )
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './testimonials.css'
+import PIC from '../../assets/me-about.jpg'
 
 const Testimonials = () => {
   return (
-    <section id='testimonial'>testimanial</section>
+    <section id='testimonial'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className='container testimanials__container'>
+        <article className='testimonial'>
+          <div className='client__avatar'>
+            <img src={PIC} alt='avatar'/>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
