1b2ac2e1f1e00d7ea0aba2fe5df27f170b83ba5a
mwaniel <mwaniel03@gmail.com>
2022-12-13 09:59:40 +0300

    creation of portfolio and its css

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,96 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/me.jpg'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>portfolio</section>
+    <section id='portfolio'>
+      <h5>My Resent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+            <div className='portfolio__item-image'>
+              <img src={IMG1} alt='' />
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <div className='portfolio__item-cta'>
+              <a href='https://github.com' className='btn' target='_blank'>Github</a>
+              <a href='https://dribble.com' className='btn btn-primary' target ='_blank'>Live demo </a>
+            </div>
+          </article>
+          <article className='portfolio__item'>
+            <div className='portfolio__item-image'>
+              <img src={IMG1} alt='' />
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <div className='portfolio__item-cta'>
+              <a href='https://github.com' className='btn' target='_blank'>Github</a>
+              <a href='https://dribble.com' className='btn btn-primary' target ='_blank'>Live demo </a>
+            </div>
+          </article>
+          <article className='portfolio__item'>
+            <div className='portfolio__item-image'>
+              <img src={IMG1} alt='' />
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <div className='portfolio__item-cta'>
+              <a href='https://github.com' className='btn' target='_blank'>Github</a>
+              <a href='https://dribble.com' className='btn btn-primary' target ='_blank'>Live demo </a>
+            </div>
+          </article>
+          <article className='portfolio__item'>
+            <div className='portfolio__item-image'>
+              <img src={IMG1} alt='' />
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <div className='portfolio__item-cta'>
+              <a href='https://github.com' className='btn' target='_blank'>Github</a>
+              <a href='https://dribble.com' className='btn btn-primary' target ='_blank'>Live demo </a>
+            </div>
+          </article>
+          <article className='portfolio__item'>
+            <div className='portfolio__item-image'>
+              <img src={IMG1} alt='' />
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <div className='portfolio__item-cta'>
+              <a href='https://github.com' className='btn' target='_blank'>Github</a>
+              <a href='https://dribble.com' className='btn btn-primary' target ='_blank'>Live demo </a>
+            </div>
+          </article>
+          <article className='portfolio__item'>
+            <div className='portfolio__item-image'>
+              <img src={IMG1} alt='' />
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <div className='portfolio__item-cta'>
+              <a href='https://github.com' className='btn' target='_blank'>Github</a>
+              <a href='https://dribble.com' className='btn btn-primary' target ='_blank'>Live demo </a>
+            </div>
+          </article>
+          <article className='portfolio__item'>
+            <div className='portfolio__item-image'>
+              <img src={IMG1} alt='' />
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <div className='portfolio__item-cta'>
+              <a href='https://github.com' className='btn' target='_blank'>Github</a>
+              <a href='https://dribble.com' className='btn btn-primary' target ='_blank'>Live demo </a>
+            </div>
+          </article>
+          <article className='portfolio__item'>
+            <div className='portfolio__item-image'>
+              <img src={IMG1} alt='' />
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <div className='portfolio__item-cta'>
+              <a href='https://github.com' className='btn' target='_blank'>Github</a>
+              <a href='https://dribble.com' className='btn btn-primary' target ='_blank'>Live demo </a>
+            </div>
+          </article>
+      </div>
+    </section>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,127 @@
 import React from 'react'
 import './services.css'
+import { BiCheck } from 'react-icons/bi';
 
 const Services = () => {
   return (
-    <section id='services'>services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className='container services__container'>
+        <article className='service'>
+          <div className='service__head'>
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className='servive__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+          </ul>
+        </article>
+        {/*end of ux ui*/}
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Web Design</h3>
+          </div>
+
+          <ul className='servive__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+          </ul>
+        </article>
+        {/*end of web*/}
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Backend development </h3>
+          </div>
+
+          <ul className='servive__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>lorem ,ipsumadaehdbabhghhsauiiiahshttasb   ajgsuahsuvaudghbjbsaughasg</p>
+            </li>
+          </ul>
+        </article>
+        {/*end of backend*/}
+      </div>
+    </section>
   );
 }
 
