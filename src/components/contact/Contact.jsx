63b6dd603acd2569c4de9c79d7b5b553d2540e3e
mwaniel <mwaniel03@gmail.com>
2022-12-13 13:30:35 +0300

    footer section , fix contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import { RiMessageLine } from 'react-icons/ri'
+import { BsWhatsapp } from 'react-icons/bs'
 
 const Contact = () => {
   return (
-    <section id='contact'>contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>mwaniel03@gmail.com</h5>
+            <a href='mailto:mwaniel03@gmail.com' target='_blank'>Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <RiMessageLine className='contact__option-icon'/>
+            <h4>messager</h4>
+            <h5>mwaniel</h5>
+            <a href='#' target='_blank'>Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>Whatsapp</h4>
+            <h5>+254702247235</h5>
+            <a href='https://api.whatsapp.com/send?phone=+254702247235' target='_blank'>Send a message</a>
+          </article>
+        </div>
+        {/* end of contact options*/}
+        <form action="">
+          <input type='text' name='name' placeholder='Your Full Name' required/>
+          <input type='email' name='email' placeholder='Your Email' required/>
+          <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   );
 }
 
-export default Contact;
+export default Contact
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { FaFacebook } from 'react-icons/fa'
+import { FiInstagram } from 'react-icons/fi'
 import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='#' className='footer__logo'>Daniel</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'> Experience</a></li>
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#testimonial'>Testimonial</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href='#'><FaFacebook/></a>
+        <a href='#' ><FiInstagram/></a>
+        <a href='#'><IoLogoTwitter/></a>
+      </div>
+
+      <div className='footer__copyright'>
+        <small>&copy; Daniel Mwaniki. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
