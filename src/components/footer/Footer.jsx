9df581d553979086033f4b798e14ff63c8b0e655
mwaniel <mwaniel03@gmail.com>
2022-12-13 14:39:50 +0300

    footer adding

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { FaFacebook } from 'react-icons/fa'
 import { FiInstagram } from 'react-icons/fi'
 import './footer.css'
+import {IoLogoTwitter} from 'react-icons/io'
 
 const Footer = () => {
   return (
